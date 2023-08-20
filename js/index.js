f9f4bf56a5d4563577b91ec81aa342f8693497db
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-08-20 16:42:01 +0500

    main section full finished ui and js

--- js/index.js
+++ js/index.js
@@ -17,66 +17,22 @@ const retry0 = document.querySelector(".retry0");
 const btnTasbeh = document.querySelector(".btnTasbeh");
 const JamiNumbers = document.querySelector(".Jami");
 const tasbehNum = document.querySelector(".tasbehNum");
+const shiorText = document.querySelector("fieldset");
 let count = 0;
 let allCount = 0;
 
-//================>> Tab Menu Content <<===================//
-allTabs.forEach((tab, idx) => {
-    tab.addEventListener("click", (e) => {
-      allTabs.forEach((tab) => tab.classList.remove("active"));
-      tab.classList.add("active");
-  
-      buttomLine.style.width = e.target.offsetWidth + "px";
-      buttomLine.style.left = e.target.offsetLeft + "px";
-  
-      allContents.forEach((content) => content.classList.remove("active"));
-      allContents[idx].classList.add("active");
-    });
-  });
-
-//===============>> Main menu <<========================//
-setInterval(() => {
-  const nowTime = new Date();
-  currentTime.textContent = `Current Time: ${nowTime.toLocaleTimeString()}`;
-});
-btnTasbeh.addEventListener("click", () => {
-  count++;
-  allCount++
-
-  retry0.addEventListener("click", () => {
-    allCount = 0;
-    JamiNumbers.textContent = `Jami: ${0}`;
-    btnTasbeh.textContent = 0;
-    tasbehNum.textContent = `${0}/33`;
-    count = 0;
-  });
-
-  JamiNumbers.textContent = `Jami: ${allCount}`;
-  btnTasbeh.textContent = count;
-  tasbehNum.textContent = `${count}/33`;
-
-  if (count === 33) {
-    count = 0;
-    btnTasbeh.textContent = 0;
-    tasbehNum.textContent = `${count}/33`
-    JamiNumbers.textContent =`Jami: ${allCount}` 
-  }
-})
-//====================================================================//
 //===========>========> Listen Qur'on Content <<=================//
-  (async () => {
-    const { data } = await customFetch(`${API_URL}/edition/format/audio`);
-    console.log(data);
-    data?.forEach((name) => {
-      const { identifier, englishName } = name;
-      chooseDomla.appendChild(
-        createOption({ value: identifier, textContent: englishName })
-      );
-    });
-  }
-)();
+(async () => {
+  const { data } = await customFetch(`${API_URL}/edition/format/audio`);
+  console.log(data);
+  data?.forEach((name) => {
+    const { identifier, englishName } = name;
+    chooseDomla.appendChild(
+      createOption({ value: identifier, textContent: englishName })
+    );
+  });
+})();
 
-// function foo(){
 chooseDomla.addEventListener("change", (e) => {
   chooseSurah.innerHTML = `<option selected disabled>Surani Tanlang</option>`;
 
@@ -123,7 +79,55 @@ chooseDomla.addEventListener("change", (e) => {
       chooseAyah.style.display = "none";
       audio.style.display = "block";
       nextBtn.style.display = "none";
+      shiorText.style.display = "none";
     });
   })();
 });
+//=====================================================================//
+
+//================>> Tab Menu Content <<===================//
+allTabs.forEach((tab, idx) => {
+  tab.addEventListener("click", (e) => {
+    allTabs.forEach((tab) => tab.classList.remove("active"));
+    tab.classList.add("active");
+
+    buttomLine.style.width = e.target.offsetWidth + "px";
+    buttomLine.style.left = e.target.offsetLeft + "px";
+
+    allContents.forEach((content) => content.classList.remove("active"));
+    allContents[idx].classList.add("active");
+  });
+});
+//==========================================================//
+
+//===============>> Main menu <<========================//
+setInterval(() => {
+  const nowTime = new Date();
+  currentTime.textContent = `Current Time: ${nowTime.toLocaleTimeString()}`;
+});
+btnTasbeh.addEventListener("click", () => {
+  count++;
+  allCount++;
+
+  retry0.addEventListener("click", () => {
+    allCount = 0;
+    JamiNumbers.textContent = `Jami: ${0}`;
+    btnTasbeh.textContent = 0;
+    tasbehNum.textContent = `${0}/33`;
+    count = 0;
+  });
+
+  JamiNumbers.textContent = `Jami: ${allCount}`;
+  btnTasbeh.textContent = count;
+  tasbehNum.textContent = `${count}/33`;
+
+  if (count === 33) {
+    count = 0;
+    btnTasbeh.textContent = 0;
+    tasbehNum.textContent = `${count}/33`;
+    JamiNumbers.textContent = `Jami: ${allCount}`;
+  }
+});
+//====================================================================//
+
 //===============>> Settings Content <<==================//

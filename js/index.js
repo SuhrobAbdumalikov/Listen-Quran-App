2f115f59bc4d6a764fee5177ff01cd1bca099225
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-08-21 01:14:28 +0500

    settings section full finished

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ import { API_URL } from "./dataApi.js";
 import { createOption } from "./ui.js";
 import { customFetch } from "./utils.js";
 
+const quranAppBox = document.querySelector(".quranAppBox");
 const chooseDomla = document.querySelector("#domla");
 const chooseSurah = document.querySelector("#surah");
 const chooseAyah = document.querySelector("#ayah");
 const nextBtn = document.querySelector("#nextBtn");
 const backAyah = document.querySelector("#backAyah");
+const nightBtn = document.querySelector(".nightBtn");
 const audio = document.querySelector("audio");
 const oyatText = document.querySelector("#text");
 const allTabs = document.querySelectorAll(".tab_btn");
@@ -18,8 +20,9 @@ const btnTasbeh = document.querySelector(".btnTasbeh");
 const JamiNumbers = document.querySelector(".Jami");
 const tasbehNum = document.querySelector(".tasbehNum");
 const shiorText = document.querySelector("fieldset");
-let count = 0;
-let allCount = 0;
+const toggleBtn = document.querySelector(".togglebtn");
+const Info = document.querySelector(".Info");
+const aboutAppPopSection = document.querySelector(".aboutAppPopSection");
 
 //===========>========> Listen Qur'on Content <<=================//
 (async () => {
@@ -107,17 +110,24 @@ setInterval(() => {
 });
 const clickAudio = new Audio();
 clickAudio.src = "../audio/click2.mp3";
+let count = 0;
+let allCount = 0;
 
 btnTasbeh.addEventListener("click", () => {
   count++;
   allCount++;
   clickAudio.play();
   retry0.addEventListener("click", () => {
+    localStorage.clear();
+    // localStorage.removeItem("countBtnNum")
+    // localStorage.removeItem("countAllNum")
+    //   JSON.parse(localStorage.removeItem("countAllNum"));
+    //   JSON.parse();
+    count = 0;
     allCount = 0;
     JamiNumbers.textContent = `Jami: ${0}`;
     btnTasbeh.textContent = 0;
     tasbehNum.textContent = `${0}/33`;
-    count = 0;
   });
 
   JamiNumbers.textContent = `Jami: ${allCount}`;
@@ -125,12 +135,74 @@ btnTasbeh.addEventListener("click", () => {
   tasbehNum.textContent = `${count}/33`;
 
   if (count === 33) {
+    allCount++;
     count = 0;
     btnTasbeh.textContent = 0;
     tasbehNum.textContent = `${count}/33`;
     JamiNumbers.textContent = `Jami: ${allCount}`;
   }
+  localStorage.setItem("countBtnNum", JSON.stringify(count));
+  localStorage.setItem("countAllNum", JSON.stringify(allCount));
 });
+
+let getAllNumCount = JSON.parse(localStorage.getItem("countAllNum"));
+let getBtnTasCount = JSON.parse(localStorage.getItem("countBtnNum"));
+JamiNumbers.textContent = `Jami: ${getAllNumCount}`;
+btnTasbeh.textContent = getBtnTasCount;
+tasbehNum.textContent = `${getBtnTasCount}/33`;
 //====================================================================//
 
 //===============>> Settings Content <<==================//
+toggleBtn.addEventListener("click", () => {
+  quranAppBox.classList.toggle("dark");
+  toggleBtn.classList.toggle("active");
+  nightBtn.classList.toggle("nightMode");
+  Info.classList.toggle("nightMode");
+  aboutAppPopSection.classList.toggle("dark");
+  let theme;
+
+  if (
+    quranAppBox.classList.contains("dark") &&
+    nightBtn.classList.contains("nightMode")
+  ) {
+    console.log("dark");
+    theme = "DARK";
+  } else {
+    console.log("light");
+    theme = "LIGHT";
+  }
+
+  localStorage.setItem("pageMode", JSON.stringify(theme));
+});
+
+let getTheme = JSON.parse(localStorage.getItem("pageMode"));
+console.log(getTheme);
+
+if (getTheme === "DARK") {
+  quranAppBox.classList.add("dark");
+  nightBtn.classList.add("nightMode");
+  toggleBtn.classList.add("active");
+  Info.classList.add("nightMode");
+  aboutAppPopSection.classList.add("dark");
+}
+
+// =====>> Info <<===//
+Info.addEventListener("click", () => {
+  const aboutAppP = document.querySelectorAll(".aboutAppP");
+  const chevronBack = document.querySelector("#chevronBack");
+  aboutAppPopSection.style.width = "100%";
+  setTimeout(() => {
+    aboutAppP.forEach((e) => {
+      e.style.opacity = 1;
+    });
+  }, 1000);
+  chevronBack.addEventListener("click", () => {
+    aboutAppPopSection.style.width = "0%";
+    setTimeout(() => {
+      aboutAppP.forEach((e) => {
+        e.style.opacity = 0;
+      });
+    }, 1000);
+  });
+});
+//===========================================================//
